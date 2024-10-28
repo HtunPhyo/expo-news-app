c84eb1f2bfe31efdb45a9e9a8d80e009adab5ae5
HtunPhyo <destinyindiegames@gmail.com>
2024-10-28 18:55:59 +0630

    welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,85 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import Animated, { FadeInDown, FadeInRight } from "react-native-reanimated";
+import { StatusBar } from "expo-status-bar";
 
-const Page = () => {
+export default function Index() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
+    <View style={styles.mainContainer}>
+      <StatusBar style="light" />
+      <ImageBackground
+        source={require("../assets/images/getting-started.jpg")}
+        resizeMode="cover"
+        style={styles.imageBackground}
+      >
+        <View style={styles.textContainer}>
+          <Animated.Text
+            entering={FadeInRight.delay(300).duration(500)}
+            style={styles.title}
+          >
+            Stay updated
+          </Animated.Text>
+          <Text style={styles.description}>with the latest news</Text>
+          <Animated.View entering={FadeInDown.delay(1000).duration(500)}>
+            <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Getting Started</Text>
+              </View>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </ImageBackground>
     </View>
   );
-};
-
-export default Page;
+}
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  imageBackground: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  textContainer: {
+    flex: 1,
+    gap: 10,
+    paddingBottom: 40,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  description: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  button: {
+    width: "100%", // Set width to 100%
+    maxWidth: 700, // Max width of 700 pixels
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "white",
+    alignSelf: "center",
   },
 });
